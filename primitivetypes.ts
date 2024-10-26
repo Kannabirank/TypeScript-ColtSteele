9fe21279943b7a6ad8d968297d2507867794ec5e
Kannan Karmegam <kannabirank@yahoo.com>
2024-10-26 23:05:26 +1000

    k
    k

--- primitivetypes.ts
+++ primitivetypes.ts
@@ -16,7 +16,58 @@ thing = true
 thing.toUpperCase()
 
 const agentSkillSets = ['Karate', 'Hacking']
-let foundSkill;
+let foundSkill
 for (let agentSkillSet of agentSkillSets) {
-  
 }
+
+//function parameter
+function assignAgentTask(personToKill: string) {}
+
+assignAgentTask('Kannan')
+//assignAgentTask(100)
+
+//arrow function, function parameter, default values
+const doSomething = (
+  person: string,
+  age: number,
+  isDump: boolean,
+  isRich: string = 'super rich'
+) => {}
+
+//function return type
+function greet(): string {
+  return 'hello dude'
+}
+
+//anonymous functions.. incomplete
+const listOfColours = ['red', 'blue', 'orange']
+
+//void
+function printTwice(message: string): void {
+  console.log(message)
+}
+
+//never - confusing
+function error(message: string): never {
+  throw new Error(message)
+}
+
+// Inferred return type is never
+function fail() {
+  return error('Something failed')
+}
+
+//type union
+function calculateTax(amount: number, format: boolean): string | number {
+  const calcAmount = amount * 1.38
+  return format ? `$${calcAmount.toFixed(2)}` : calcAmount
+}
+
+//type assertion
+let taxNumber = calculateTax(100, false) as number
+let taxString = calculateTax(500, true) as number
+
+let taxString1 = <string>calculateTax(100, false)
+let taxString2 = calculateTax(100, false) as string
+
+//unknown type
